2d58a83c529f7960d9470c4df883b044d929946e
Felix Labelle <flx.labelle@gmail.com>
2019-04-10 18:52:38 -0400

    Working, but having an issue displaying with unicode

--- pdf_to_speech.py
+++ pdf_to_speech.py
@@ -4,6 +4,9 @@ import time
 import xml.etree.ElementTree as ET
 import re
 import argparse
+import os
+import io
+import sys
 # TODO: Make it cross system by removing dependency on comtypes
 # (https://stackoverflow.com/questions/39014468/how-to-save-the-output-of-pyttsx-to-wav-file)
 # https://pypi.org/project/pyttsx3/ (looks unsupported..)
@@ -11,37 +14,60 @@ import argparse
 # https://www.w3.org/TR/speech-synthesis/
 # TODO: Add way to easily modify the voice 
 class SSML_Generator:
-	def __init__(self,pause):
+	def __init__(self,pause,phonemeFile):
 		self.pause = pause
+		if isinstance(phonemeFile,str):
+			print("Loading dictionary")
+			self.phonemeDict = self._load_phonemes(phonemeFile)
+			print(len(self.phonemeDict))
+		else:
+			self.phonemeDict = {}
 		
+	def _load_phonemes(self, phonemeFile):
+		phonemeDict = {}
+		with io.open(phonemeFile, 'r',encoding='utf-8') as f:
+			for line in f:
+				tok = line.split()
+				#print(len(tok))
+				phonemeDict[tok[0].lower()] = tok[1].lower()
+		return phonemeDict
+	
 	def __call__(self,text):
 		SSML_document = self._header()
-		SSML_document.text = text[0]
-		for utterance in text[1:]:
-			brk = self._pause(SSML_document)
-			brk.tail = utterance
+		for utterance in text:
+			parent_tag = self._pronounce(utterance,SSML_document)
+			#parent_tag.tail = self._pause(parent_tag)
+			SSML_document.append(parent_tag)
+		ET.dump(SSML_document)
 		return SSML_document
 		
 	def _pause(self,parent_tag):
-		return ET.SubElement(parent_tag,"break",{"time":str(self.pause)+"ms"})
+		return 	ET.fromstring("<break time=\"150ms\" />") # ET.SubElement(parent_tag,"break",{"time":str(self.pause)+"ms"})
 
 	def _header(self):
 		return ET.Element("speak",{"version":"1.0", "xmlns":"http://www.w3.org/2001/10/synthesis", "xml:lang":"en-US"})
 	
 	# TODO: Add rate https://docs.microsoft.com/en-us/cortana/skills/speech-synthesis-markup-language#prosody-element
-	def _rate(self)
+	def _rate(self):
 		pass
 	
 	# TODO: Add pitch 
-	def _pitch(self)
+	def _pitch(self):
 		pass
+	
+	def _pronounce(self,word,parent_tag):
+		if word in self.phonemeDict:
+			sys.stdout.buffer.write(self.phonemeDict[word].encode("utf-8"))
+			return ET.fromstring("<phoneme alphabet=\"ipa\" ph=\"" + self.phonemeDict[word] + "\"> </phoneme>")#ET.SubElement(parent_tag,"phoneme",{"alphabet":"ipa","ph":self.phonemeDict[word]})#<phoneme alphabet="string" ph="string"></phoneme>
+		else:
+			return parent_tag
 	# Nice to have: Transform acronyms into their pronunciation (See say as tag)
 	
 class SpeechToWav:
-	def __init__(self,pause,engineRate):
+	def __init__(self,pause,engineRate,phonemeFile):
 		self.engine = CreateObject("SAPI.SpVoice")
 		self.stream = CreateObject("SAPI.SpFileStream")
-		self.SSML_generator = SSML_Generator(pause)
+		self.SSML_generator = SSML_Generator(pause,phonemeFile)
 		# This import has to be done in a specific order, otherwise Windows is not happy
 		from comtypes.gen import SpeechLib
 		# Because of that, below is a work around to use its functions throughout the object.
@@ -65,7 +91,7 @@ class SpeechToWav:
 	
 	def _text_processing(self,text):
 		""" Used to remove side effects from columns, the new lines add bizzare pauses """
-		return re.findall(r"[\w']+", text)
+		return re.findall(r"[\w']+", text.lower())
 
 class TextExtractor:
 	def __init__(self):
@@ -84,6 +110,7 @@ class TextExtractor:
 def parser():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-f', '--file',required=True)
+	parser.add_argument('-i', '--phonemeFile',nargs = '?',default = None)
 	parser.add_argument('-r', '--engineRate', nargs = '?', type=int,default = -2)
 	parser.add_argument('-p', '--pause', nargs = '?', type=int, default = 150)
 	args = parser.parse_args()
@@ -91,9 +118,16 @@ def parser():
 	input_file = args.file
 	output_file = input_file
 	pdfExtractor = TextExtractor()
-	audioConverter = SpeechToWav(args.pause,args.engineRate)
+	audioConverter = SpeechToWav(args.pause,args.engineRate,args.phonemeFile)
 	audioConverter(text = pdfExtractor(inputFile = input_file),outputFile = output_file)
 	
 if __name__ == '__main__':
-	parser()
+	#parser()
+	#input_file = args.file
+	output_file = "hwet"
+	#pdfExtractor = TextExtractor()
+	cwd = os.getcwd()
+	phonetic_lib_dir = os.path.join(cwd,"cmudict-ipa")
+	audioConverter = SpeechToWav(150,-2,os.path.join(phonetic_lib_dir,"cmudict-0.7b-ipa.txt"))
+	audioConverter(text = "Hello world extended test",outputFile = output_file)
 	
