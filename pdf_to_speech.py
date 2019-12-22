d63cd9ca041145dedbb42c48333165a8d749c85b
Felix Labelle <flx.labelle@gmail.com>
2019-12-22 09:27:50 -0500

    Swapped out pypdf2, fixed wrap around errors, working on pronounciation and encoding stuff(very choppy audio)

--- pdf_to_speech.py
+++ pdf_to_speech.py
@@ -1,5 +1,9 @@
 from comtypes.client import CreateObject
-import PyPDF2
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
 import time
 import xml.etree.ElementTree as ET
 import re
@@ -7,6 +11,8 @@ import argparse
 import os
 import io
 import sys
+from nltk.tokenize import sent_tokenize
+
 # TODO: Make it cross system by removing dependency on comtypes
 # (https://stackoverflow.com/questions/39014468/how-to-save-the-output-of-pyttsx-to-wav-file)
 # https://pypi.org/project/pyttsx3/ (looks unsupported..)
@@ -40,8 +46,8 @@ class SSML_Generator:
 		ET.dump(SSML_document)
 		return SSML_document
 		
-	def _pause(self,parent_tag):
-		return 	ET.fromstring("<break time=\"150ms\" />") # ET.SubElement(parent_tag,"break",{"time":str(self.pause)+"ms"})
+	def _pause(self,parent_tag, pause_time):
+		return 	ET.fromstring("<break time=\"" + pause_time + "ms\" />") # ET.SubElement(parent_tag,"break",{"time":str(self.pause)+"ms"})
 
 	def _header(self):
 		return ET.Element("speak",{"version":"1.0", "xmlns":"http://www.w3.org/2001/10/synthesis", "xml:lang":"en-US"})
@@ -60,17 +66,45 @@ class SSML_Generator:
 			#print(word, self.phonemeDict[word])
 			return ET.fromstring('<phoneme alphabet="ipa" ph=\'' + self.phonemeDict[word] + '\'> </phoneme>')#ET.SubElement(parent_tag,"phoneme",{"alphabet":"ipa","ph":self.phonemeDict[word]})#<phoneme alphabet="string" ph="string"></phoneme>
 		else:
-			return self.
+			return ET.fromstring('<s>' + word +'</s>')
 	# Nice to have: Transform acronyms into their pronunciation (See say as tag)
+
+class TextProcesser:
+	def __init__(self,type):
+		self.type = type
+		self.preprocessText = self._preprocessText
+		self.tokenizeText = self._wordTokenizer
+		if type == "sent":
+			self.tokenizeText = sent_tokenize
+		elif type == "word":
+			self.tokenizeText = self._wordTokenizer
 	
-class SpeechToWav:
+	def _wrapAround(self, text):
+		return text.replace('-\n','')
+		
+	def _removeNewlines(self, text):
+		return text.replace('\n',' ')
+
+	def _wordTokenizer(self, text):
+		return re.findall(r"[\w']+", text)
+	
+	def _preprocessText(self, text):
+		text = self._wrapAround(text)
+		return text.lower()
+	
+	def __call__(self,text):
+		text = self.preprocessText(text)
+		return self.tokenizeText(text)
+
+class TTS:
 	def __init__(self,pause,engineRate,phonemeFile):
 		self.engine = CreateObject("SAPI.SpVoice")
 		self.stream = CreateObject("SAPI.SpFileStream")
 		self.SSML_generator = SSML_Generator(pause,phonemeFile)
+		self.textProcesser = TextProcesser("word")
 		# This import has to be done in a specific order, otherwise Windows is not happy
 		from comtypes.gen import SpeechLib
-		# Because of that, below is a work around to use its functions throughout the object.
+		# Because of that, below is a workaround to use SpeechLib throughout the object.
 		self.SpeechLib = SpeechLib
 		self.engine.Rate = engineRate
 	
@@ -82,30 +116,39 @@ class SpeechToWav:
 		# https://docs.microsoft.com/en-us/previous-versions/windows/desktop/ms723606(v%3Dvs.85)
 		self.stream.Open(outputFile + ".wav", self.SpeechLib.SSFMCreateForWrite)
 		self.engine.AudioOutputStream = self.stream
-		text = self._text_processing(text)
+		import pdb;pdb.set_trace()
+		text = self.textProcesser(text)
 		text = self.SSML_generator(text)
 		text = ET.tostring(text,encoding='utf8', method='xml').decode('utf-8')
 		self.engine.speak(text)
 		self.stream.Close()
-	
-	
-	def _text_processing(self,text):
-		""" Used to remove side effects from columns, the new lines add bizzare pauses """
-		return re.findall(r"[\w']+", text.lower())
 
 class TextExtractor:
 	def __init__(self):
 		pass
 		
-	def __call__(self,inputFile):
-		# TODO: Find good ways to identify the layout and different sections
-		# TODO: Better recognize when the citations are going to occur and cut them out
-		pdfFileObj = open(inputFile + '.pdf','rb')	 #'rb' for read binary mode
-		pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-		extractedText = """"""
-		for page in range(0,pdfReader.numPages):
-			extractedText += pdfReader.getPage(page).extractText()
-		return extractedText
+	def __call__(self, path):
+		rsrcmgr = PDFResourceManager()
+		retstr = StringIO()
+		codec = 'utf-8'
+		laparams = LAParams()
+		device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+		fp = open(path, 'rb')
+		interpreter = PDFPageInterpreter(rsrcmgr, device)
+		password = ""
+		maxpages = 0
+		caching = True
+		pagenos=set()
+
+		for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+			interpreter.process_page(page)
+
+		text = retstr.getvalue()
+
+		fp.close()
+		device.close()
+		retstr.close()
+		return text
 
 def parser():
 	parser = argparse.ArgumentParser()
@@ -114,12 +157,11 @@ def parser():
 	parser.add_argument('-r', '--engineRate', nargs = '?', type=int,default = -2)
 	parser.add_argument('-p', '--pause', nargs = '?', type=int, default = 150)
 	args = parser.parse_args()
-
 	input_file = args.file
 	output_file = input_file
 	pdfExtractor = TextExtractor()
-	audioConverter = SpeechToWav(args.pause,args.engineRate,args.phonemeFile)
-	audioConverter(text = pdfExtractor(inputFile = input_file),outputFile = output_file)
+	audioConverter = TTS(args.pause,args.engineRate,args.phonemeFile)
+	audioConverter(text = pdfExtractor(input_file),outputFile = output_file)
 	
 if __name__ == '__main__':
 	parser()
